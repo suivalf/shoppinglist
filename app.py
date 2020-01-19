93868c862f778098c23ce2358cef6f45c5ce89bf
suivalf <cirstian_flaviusmihai@yahoo.com>
2020-01-19 15:59:04 +0200

    Minor changes

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ class Item(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     item_name = db.Column(db.String(200), nullable=False)
     item_qty = db.Column(db.Integer, nullable=True)
+    def __init__(self, item_name, item_qty):
+        self.item_name = item_name
+        self.item_qty = item_qty
     def __repr__(self):
         return '<Item: %r, Name: %r, Quantity: %r>' % self.id % self.item_name % self.item_qty
 
